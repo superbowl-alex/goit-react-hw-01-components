93439da67ecb82a4c645c157bb177c5437f277b1
Oleksandr <superbowl@ukr.net>
2022-09-23 09:47:47 +0300

    Task02 styled

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,11 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && (
+        <div className={css.titleWrapper}>
+          <h2 className={css.title}>{title}</h2>
+        </div>
+      )}
       <StatList statObjects={stats} />
     </section>
   );
